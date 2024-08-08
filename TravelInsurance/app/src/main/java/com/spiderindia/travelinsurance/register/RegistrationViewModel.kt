7a51a60ed421f31a6d1ebbd67a3d5ca65f807105
VigneshMathav <vickymathavan13@gmail.com>
2024-08-08 23:43:38 +0530

    Resource and Register Handle Thread

--- TravelInsurance/app/src/main/java/com/spiderindia/travelinsurance/register/RegistrationViewModel.kt
+++ TravelInsurance/app/src/main/java/com/spiderindia/travelinsurance/register/RegistrationViewModel.kt
@@ -3,16 +3,24 @@ package com.spiderindia.travelinsurance.register
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.spiderindia.travelinsurance.R
 import com.spiderindia.travelinsurance.model.mbo.User
 import com.spiderindia.travelinsurance.model.mbo.UserError
+import com.spiderindia.travelinsurance.model.repository.TravelRepository
+import com.spiderindia.travelinsurance.util.Resource
+import com.spiderindia.travelinsurance.util.toSHA256Hash
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 
-class RegistrationViewModel : ViewModel() {
+class RegistrationViewModel(private val repository: TravelRepository) : ViewModel() {
 
     private val _liveUserData = MutableLiveData<User>(User())
     val liveUserData: LiveData<User> = _liveUserData
     private val _liveUserError = MutableLiveData<UserError>(UserError())
     val liveUserError: LiveData<UserError> = _liveUserError
+    val liveRegisterFlow = MutableLiveData<Resource.Status>(Resource.Status.NONE)
 
     fun isValidInput(confirmPassword: String): Boolean {
         var isValid = true
@@ -54,6 +62,36 @@ class RegistrationViewModel : ViewModel() {
         if (isValid && confirmPassword != liveUserData.value?.password) {
             _liveUserError.postValue(UserError(confirmpassword =  R.string.msg_error_passwordnotmatch))
             isValid = false
+        }
+
+        if (isValid)
+        {
+            _liveUserError.postValue(UserError())
+            viewModelScope.launch(IO) {
+                val lstUser = repository.findUserByUserName(liveUserData.value!!.username,
+                    liveUserData.value!!.email)
+
+                if(lstUser?.isNotEmpty() == true)
+                {
+                    if (lstUser.find { user -> user.email == liveUserData.value!!.email} != null)
+                    {
+                        _liveUserError.postValue(UserError(email = R.string.msg_error_email_already_exist))
+                    }
+                    else
+                    {
+                        _liveUserError.postValue(UserError(username = R.string.msg_error_username_already_exist))
+                    }
+                }
+                if (lstUser?.isEmpty() == true)
+                {
+                    liveRegisterFlow.postValue(Resource.Status.LOADING)
+                    val userInfo = liveUserData.value!!.copy(password = liveUserData.value!!.password.toSHA256Hash())
+                    repository.doRegister(userInfo)
+                    liveRegisterFlow.postValue(Resource.Status.SUCCESS)
+                }
+
+            }
+
         }
 
 
